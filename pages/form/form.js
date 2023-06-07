c355c0047ad1a68a73c1ad0629e46352aa07b31c
hadar gueta <hadart20@gmail.com>
2023-06-08 00:40:54 +0300

    add sharp

--- pages/form/form.js
+++ pages/form/form.js
@@ -1,10 +1,12 @@
 import Image from 'next/image';
-import FormImage from '../../public/hero02.jpeg'; // Corrected import statement
+import FromImage from '../../public/hero02.jpeg';
 import { useState } from 'react';
+
 
 export function Form() {
     const [lead, setLead] = useState({});
     const [submissionStatus, setSubmissionStatus] = useState(null);
+
 
     const handleButtonClick = async (e) => {
         e.preventDefault();
@@ -29,6 +31,7 @@ export function Form() {
         }
     };
 
+
     const handleInputChange = (e) => {
         setLead((prevLead) => ({
             ...prevLead,
@@ -39,9 +42,58 @@ export function Form() {
     return (
         <section className="text-center bg-#faf9f8 relative h-2/3 py-16 p-12 " id='form-section'>
             <div className="absolute inset-0">
-                <Image src={FormImage} alt="form" layout="fill" objectFit="cover" className="opacity-20" />
+                <Image src={FromImage} alt="form" layout="fill" objectFit="cover" className="opacity-20" />
             </div>
-            {/* Rest of the code */}
+            <div className="relative h-full flex flex-col justify-center">
+                <h2 className="text-3xl font-bold mb-4 text-green-900 relative hover:text-green-500 hover:scale-105 transition-all">
+                    השאירו פרטים
+                </h2>
+                <form className="mt-8">
+                    {/* Form inputs */}
+                    <div className="flex flex-col md:flex-row justify-center">
+                        <input
+                            type="text"
+                            name="name"
+                            placeholder="Name"
+                            className="bg-white py-2 px-4 mb-4 md:mr-4 rounded-lg shadow-lg"
+                            onChange={handleInputChange}
+                        />
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Email"
+                            className="bg-white py-2 px-4 mb-4 md:mr-4 rounded-lg shadow-lg"
+                            onChange={handleInputChange}
+                        />
+                        <input
+                            type="tel"
+                            name="phone"
+                            placeholder="Phone Number"
+                            className="bg-white py-2 px-4 mb-4 md:mr-4 rounded-lg shadow-lg"
+                            onChange={handleInputChange}
+                        />
+                        <button
+                            onClick={handleButtonClick}
+                            className="bg-green-600 text-white py-2 px-4 mb-4 rounded-lg hover:bg-green-400 hover:text-white"
+                        >
+                            שלח
+                        </button>
+                    </div>
+                </form>
+            </div>
+            {submissionStatus === 'success' && (
+                <div dir="rtl" className="dialog-success font-bold text-green-700">
+                    <h2>הפרטים התקבלו בהצלחה! אצור קשר בהקדם.</h2>
+                    
+                </div>
+            )}
+            {submissionStatus === 'failure' && (
+                <div dir="rtl" className="dialog-failure font-bold text-red-700">
+                    Something did not work well. Can you send again?
+                </div>
+            )}
         </section>
+
     );
 }
+
